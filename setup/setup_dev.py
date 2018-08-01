17cb86c6f2f0016d6020947952ab1b5a07fd2a09
yubinhooms <41712363+yubinhooms@users.noreply.github.com>
2018-08-01 14:57:52 +0800

    Update setup_dev.py

--- setup/setup_dev.py
+++ setup/setup_dev.py
@@ -28,7 +28,7 @@ if re.search('github.com[:/]odoo/odoo.git$', sys.argv[2]):
 """
 
 def printf(f,*l):
-    print "odoo:" + f % l
+    print ("odoo:" + f % l)
 
 def run(*l):
     if isinstance(l[0], list):
